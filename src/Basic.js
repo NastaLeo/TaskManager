f5c5caf2d0777f227eda33563678643f4b4b3fc2
sia <anastasiya6956@mail.ru>
2021-02-27 18:42:06 +0300

    fix create, mark, delete features

--- src/Basic.js
+++ src/Basic.js
@@ -1,24 +1,21 @@
-import './assets/styles/style.css';
-import rubbish from './assets/images/rubbish1.png';
-
-
 export default class Task {
 
     constructor(name, checked = false) {
+
         this.name = name;
         this.checked = checked
+
     }
 
-    create() {
-        let li = document.createElement('li');
+    create(li, rubbish) {
 
-        let checkbox = document.createElement('input');
+        const checkbox = document.createElement('input');
         checkbox.type = 'checkbox';
 
-        let liContent = document.createElement('span');
+        const liContent = document.createElement('span');
         liContent.textContent = this.name;
 
-        let deleteIcon = document.createElement('img');
+        const deleteIcon = document.createElement('img');
         deleteIcon.src = rubbish;
         deleteIcon.classList.add('delete');
 
@@ -26,80 +23,6 @@ export default class Task {
         li.append(liContent);
         li.append(deleteIcon);
 
-        let option = document.querySelectorAll('option');
-
-        if (option[0].selected) {
-            document.querySelector('div.unimportant ul').append(li);
-
-            if (Array.from(document.querySelector('div.unimportant ul').children).length > 0 && 
-            Array.from(document.querySelector('div.unimportant ul').children).some(li => li.classList.contains('clean'))) {
-                
-            li.classList.add('clean')
-            }
-        }
-
-        else if (option[1].selected) {
-            document.querySelector('div.important ul').append(li);
-
-            if (Array.from(document.querySelector('div.important ul').children).length > 0 && 
-            Array.from(document.querySelector('div.important ul').children).some(li => li.classList.contains('clean'))) {
-                
-            li.classList.add('clean')
-            }
-        }
-
-        else if (option[2].selected) {
-            document.querySelector('div.urgent ul').append(li);
-
-            if (Array.from(document.querySelector('div.urgent ul').children).length > 0 && 
-            Array.from(document.querySelector('div.urgent ul').children).some(li => li.classList.contains('clean'))) {
-                
-            li.classList.add('clean')
-            }
-        }
-    }
-
-    mark(event) {
-        if (event.target.tagName != 'INPUT') {
-            console.log('no');
-            return
-        } else {
-            if (event.target.checked) {
-                console.log('true');
-                event.target.checked = true;
-                event.target.setAttribute('checked', true);
-            } else {
-                console.log('false');
-                event.target.checked = false;
-                event.target.setAttribute('checked', false);
-            }
-        }
-    }
-
-
-    hideShow(event) {
-        if (!event.target.classList.contains('tasks')) {
-            console.log(4)
-            return
-        } else {
-            for (let li of Array.from(event.target.nextElementSibling.children)){
-                 console.log(li)
-                if (!li.firstElementChild.checked) {
-                   console.log(2);
-                   li.classList.toggle('clean');
-                   event.target.classList.toggle('chosen')
-                }
-            }
-        } 
-    }
-
-
-    delete(event) {
-        if (event.target.className === 'delete' && event.target.closest('li').firstElementChild.checked === true){
-            event.target.closest('li').remove();
-        } else {
-            return
-        }
     }
     
 }
--- src/index.js
+++ src/index.js
@@ -1,47 +1,56 @@
-import './assets/styles/style.css';
+import {openForm, closeForm, submitTask, markTask, deleteTask} from './Actions.js';
+
 import planner from './assets/images/planner.png';
-import {buttonAdd, openForm, close, closeForm, submitTask} from './Actions.js';
-import Task from './Basic.js';
-import ImportantTask from "./Important.js";
-import UnimportantTask from "./Unimportant.js";
-import UrgentTask from "./Urgent.js";
+import cross from './assets/images/cross.png';
+import rubbish from './assets/images/rubbish1.png';
+
+
+import './assets/styles/style.css';
+
+
+const favicon = document.querySelector('link[rel=icon]');
+const addButton = document.querySelector('button[data-action=add]');
+const addList = document.querySelector('.add-list');
+const form = document.querySelector('form');
+const close = document.querySelector('img.cross');
+const list = document.querySelector('div.list-container');
 
 
 
 //draw favicon
-let favicon = document.querySelector('link[rel=icon]');
 favicon.href = planner;
 
+
+//draw cross in addList form
+close.src = cross;
+
+
 //show form fo fill in
-buttonAdd.addEventListener('click', openForm);
+addButton.addEventListener('click', () => openForm(addButton, addList));
+
 
 //hide form fo fill in
-close.addEventListener('click', closeForm);
+close.addEventListener('click', () => closeForm(addButton, addList));
 
-
-
-
-let form = document.querySelector('form');
 
 //submitTask
-form.addEventListener('submit', submitTask);
+form.addEventListener('submit', (event) => submitTask(event, rubbish));
 
 
+//mark checked status
+list.addEventListener('click', (event) => markTask(event));
 
 
-
-let list = document.querySelector('div.list-container');
 
 //delete task
-list.addEventListener('click', new Task().delete);
+list.addEventListener('click', (event) => deleteTask(event));
 
-//maintain checked state
-list.addEventListener('click', new Task().mark);
+
 
 //hide/show non-marked tasks
-list.addEventListener('click', new Task().hideShow);
+// list.addEventListener('click', hideShowTask());
 
 //tasks after their unmark
-list.addEventListener('click', new ImportantTask().hideTask);
-list.addEventListener('click', new UnimportantTask().hideTask);
-list.addEventListener('click', new UrgentTask().hideTask);
+// list.addEventListener('click', new ImportantTask().hideTask);
+// list.addEventListener('click', new UnimportantTask().hideTask);
+// list.addEventListener('click', new UrgentTask().hideTask);
